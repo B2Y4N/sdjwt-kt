85a709af7cd98d0b57985edd3537b2ce8b35c1e3
Bryan Chew <bryanchew@cyberlance.com.my>
2026-04-29 17:09:26 +0800

    remove sourcesJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ version = "1.0.0"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-    withSourcesJar()
 }
 
 tasks.withType<KotlinCompile> {
